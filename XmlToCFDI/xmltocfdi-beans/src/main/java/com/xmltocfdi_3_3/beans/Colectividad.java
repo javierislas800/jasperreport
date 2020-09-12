63c3ff5fc61f8d3d29ef5af589ec157ac62e02ff
Javier Islas <javierislas800@gmail.com>
2020-09-12 17:11:04 -0500

    Se muestra la tabla

---
+++ XmlToCFDI/xmltocfdi-beans/src/main/java/com/xmltocfdi_3_3/beans/Colectividad.java
@@ -0,0 +1,60 @@
+package com.xmltocfdi_3_3.beans;
+
+public class Colectividad {
+
+	private String filial;
+	private String colectividad;
+	private String primaNeta;
+	private String analogos;
+	private String derechoPoliza;
+	private String recargoFraccionado;
+	
+	public String getFilial() {
+		return filial;
+	}
+	
+	public void setFilial(String filial) {
+		this.filial = filial;
+	}
+	
+	public String getColectividad() {
+		return colectividad;
+	}
+	
+	public void setColectividad(String colectividad) {
+		this.colectividad = colectividad;
+	}
+	
+	public String getPrimaNeta() {
+		return primaNeta;
+	}
+	
+	public void setPrimaNeta(String primaNeta) {
+		this.primaNeta = primaNeta;
+	}
+	
+	public String getAnalogos() {
+		return analogos;
+	}
+	
+	public void setAnalogos(String analogos) {
+		this.analogos = analogos;
+	}
+	
+	public String getDerechoPoliza() {
+		return derechoPoliza;
+	}
+	
+	public void setDerechoPoliza(String derechoPoliza) {
+		this.derechoPoliza = derechoPoliza;
+	}
+	
+	public String getRecargoFraccionado() {
+		return recargoFraccionado;
+	}
+	
+	public void setRecargoFraccionado(String recargoFraccionado) {
+		this.recargoFraccionado = recargoFraccionado;
+	}
+	
+}
--- XmlToCFDI/xmltocfdi/src/main/java/com/xmltocfdi_3_3/ReporteColectividadesCaratula.java
+++ XmlToCFDI/xmltocfdi/src/main/java/com/xmltocfdi_3_3/ReporteColectividadesCaratula.java
@@ -3,10 +3,14 @@ package com.xmltocfdi_3_3;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
+
+import com.xmltocfdi_3_3.beans.Colectividad;
 
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -14,6 +18,7 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
@@ -29,11 +34,28 @@ public class ReporteColectividadesCaratula {
 		// Datos Carátula de Póliza
 		params.put("NOMBRE",    "Nombre del negocio.");
 		params.put("DSEMPRESA", "Nombre de la empresa.");
+		params.put("DSNEGOCI", "Nombre del contratante");
 
+		List<Colectividad> listItems = new ArrayList<Colectividad>();
+		
+		for (int i = 0; i < 5; i++) {
+			Colectividad col1 = new Colectividad();
+			col1.setAnalogos("A" + i);
+			col1.setColectividad("C" + i);
+			col1.setDerechoPoliza("D" + i);
+			col1.setFilial("F" + i);
+			col1.setPrimaNeta("PN" + i);
+			col1.setRecargoFraccionado("RF" + i);
+		
+			listItems.add(col1);
+		}
 
-		//List<String> listItems = new ArrayList<String>();
-		//JRBeanCollectionDataSource itemsJRBean = new JRBeanCollectionDataSource(listItems);
-				
+		
+		
+		
+		JRBeanCollectionDataSource colectividadesJRBean = new JRBeanCollectionDataSource(listItems);
+		params.put("CollectionColectividadBeanParam", colectividadesJRBean);		
+		
 		JasperDesign jasperDesign = JRXmlLoader.load(input);
 		JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
 		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JREmptyDataSource());
